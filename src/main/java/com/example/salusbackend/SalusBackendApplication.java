f7ae472c7a32b9d9cf4b566ca981b6d4b526b6ea
vadim2422 <breev.vadim@yandex.ru>
2023-12-22 15:45:38 +0300

    init

---
+++ src/main/java/com/example/salusbackend/SalusBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.salusbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalusBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalusBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/salusbackend/SalusBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.salusbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalusBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
