0f7b4d592185bad1a5510b10b53d4aab91b50c83
vadim2422 <breev.vadim@yandex.ru>
2023-12-23 21:36:23 +0300

    WIP: add Speaker controllers

--- src/main/java/com/omnia/salusbackend/controllers/SpeakerController.java
+++ src/main/java/com/omnia/salusbackend/controllers/SpeakerController.java
@@ -1,6 +1,7 @@
 package com.omnia.salusbackend.controllers;
 
 import com.omnia.salusbackend.repository.SpeakerRepository;
+import com.omnia.salusbackend.service.MeetService;
 import com.omnia.salusbackend.service.SubjectSpeakerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+
 @RestController
 @RequestMapping("/speaker")
 @RequiredArgsConstructor
@@ -16,10 +19,21 @@ public class SpeakerController {
 
     private final SpeakerRepository speakerRepository;
     final private SubjectSpeakerService subjectSpeakerService;
+    final private MeetService meetService;
 
     @GetMapping("/{speakerId}/allSubject")
     public ResponseEntity<?> getAllSpeaker(@PathVariable Long speakerId){
         return ResponseEntity.ok().body(subjectSpeakerService.getAllwithSpeakerId(speakerId));
+    }
+
+    @GetMapping("/{speakerId}/meets/{date}")
+    public ResponseEntity<?> getSpeakerMeets(@PathVariable Long speakerId, @PathVariable LocalDate date){
+        return ResponseEntity.ok().body(meetService.getMeetsMeetsForSpeakerByDate(speakerId, date));
+    }
+
+    @GetMapping("/{speakerId}/lecture/{date}")
+    public ResponseEntity<?> getSpeakerLecture(@PathVariable Long speakerId, @PathVariable LocalDate date){
+        return ResponseEntity.ok().body(meetService.getMeetsLectureForSpeakerByDate(speakerId, date));
     }
 
     }
