3c4ed27485e080c7307d27cd15e8b426580be8c3
vadim2422 <breev.vadim@yandex.ru>
2023-12-24 05:13:45 +0300

    WIP: add gmail

--- src/main/java/com/omnia/salusbackend/service/GmailService.java
+++ src/main/java/com/omnia/salusbackend/service/GmailService.java
@@ -8,24 +8,42 @@ import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
 @Service
 @RequiredArgsConstructor
 public class GmailService {
 
     public final JavaMailSender emailSender;
+    private final TemplateEngine templateEngine;
 
-    public void sendSimpleEmail(String toAddress, String subject, String message) {
+    public void sendSimpleEmail(String toAddress, String fio, String lector, Integer meetRange, String subject, String description, String place, String link) {
+
+        String htmlContent = templateEngine.process("temp.html", createContext(fio, lector, meetRange, subject, description, place, link));
 
         SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
         simpleMailMessage.setTo(toAddress);
-        simpleMailMessage.setSubject(subject);
-        simpleMailMessage.setText(message);
+        simpleMailMessage.setSubject(fio);
+        simpleMailMessage.setText(htmlContent);
         emailSender.send(simpleMailMessage);
     }
 
-    @Bean
+
+    private Context createContext(String fio, String lector, Integer meetRange, String subject, String description, String place, String link) {
+        Context context = new Context();
+        context.setVariable("fio", fio);
+        context.setVariable("lector", lector);
+        context.setVariable("meetRange", meetRange);
+        context.setVariable("subject", subject);
+        context.setVariable("description", description);
+        context.setVariable("place", place);
+        context.setVariable("link", link);
+        return context;
+    }
+
+//    @Bean
     public void test() {
-        sendSimpleEmail("breev.vad@gmail.com", "boba", "Ауф");
+        sendSimpleEmail("breev.vadim@yandex.ru", "fio", "lector", 23,"mwekflw", "desc", "place", "link");
     }
 }
