44b4b519f75326180aa4d02ccd283f8ff8faaf63
YHD97 <ryumkin.ad@gmail.com>
2023-12-24 09:19:52 +0300

    fix someone #16

--- src/main/java/com/omnia/salusbackend/TestController.java
+++ src/main/java/com/omnia/salusbackend/TestController.java
@@ -1,5 +1,6 @@
 package com.omnia.salusbackend;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
@@ -12,9 +13,20 @@ import java.security.Principal;
 @RestController
 @RequestMapping
 public class TestController {
-    @GetMapping
-    public ResponseEntity<?> test() {
-        DefaultOidcUser user = (DefaultOidcUser) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+//    @GetMapping
+//    public ResponseEntity<?> test() {
+//        DefaultOidcUser user = (DefaultOidcUser) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+//        return ResponseEntity.ok().body(user.getFullName());
+//    }
+@GetMapping
+public ResponseEntity<?> test() {
+    Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+    if (principal instanceof DefaultOidcUser user) {
         return ResponseEntity.ok().body(user.getFullName());
+    } else {
+        // Обработка, когда Principal не является объектом DefaultOidcUser
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
     }
+}
 }
